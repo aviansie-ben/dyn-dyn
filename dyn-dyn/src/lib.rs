cd78714533640795053230a2fa18ffeeeba47f9f
Benjamin <ben@benthomas.ca>
2022-08-12 23:08:34 -0400

    Fix README.md not being included in the dyn-dyn crate

--- dyn-dyn/src/lib.rs
+++ dyn-dyn/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!("../README.md")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![warn(missing_docs)]
 #![warn(clippy::undocumented_unsafe_blocks)]
